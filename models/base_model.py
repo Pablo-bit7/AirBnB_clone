8d7bd861d693bf95ec7ee6ea2504af8bfde89402
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-05-20 05:54:27 +0200

    Edits

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,41 @@
 """Module for BaseModel class."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """Define attributes and methods for BaseModel."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs:
+                setattr(self, 'created_at', datetime.now())
+            if 'updated_at' not in kwargs:
+                setattr(self, 'updated_at', datetime.now())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation of BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime."""
+        """Update updated_at attribute with current datetime and save to storage."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary representation of BaseModel instance."""
@@ -32,7 +49,7 @@ class BaseModel:
 
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
     print(my_model)
     my_model.save()
@@ -43,3 +60,11 @@ if __name__ == "__main__":
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
